4ee9b4f29cf80988ab19c8df3ead2f2cc697ee16
Faisal S.A <Ksa45560@gmail.com>
2022-02-03 15:45:01 +0300

    resolve #15 | symptom search screen

--- utils/styles.js
+++ utils/styles.js
@@ -1,4 +1,6 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, StatusBar } from "react-native";
+
+import { COLORS } from "./colors";
 
 export const STYLES = StyleSheet.create({
   rtlText: {
@@ -7,5 +9,23 @@ export const STYLES = StyleSheet.create({
   },
   rtlView: {
     flexDirection: "row-reverse"
+  },
+
+  mainContainer: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    backgroundColor: "#fff",
+  },
+  titleContainer: {
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    marginTop: 15,
+    marginBottom: 5,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: COLORS.primaryText,
   }
 });
