4027f8cffd5beb31fc7abb87c201f62b688c7648
Faisal S.A <Ksa45560@gmail.com>
2022-02-07 17:50:59 +0300

    refactoring

--- screens/Feedback.js
+++ screens/Feedback.js
@@ -1,63 +1,74 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
 import {
   SafeAreaView,
   StyleSheet,
   Text,
   View,
-  TextInput,
-  Button,
   ScrollView,
   KeyboardAvoidingView,
   Platform,
-  Keyboard,
-  TouchableWithoutFeedback 
 } from "react-native";
 import PropTypes from "prop-types";
-import FeedbackSvg from "../components/svg/Feedback";
-import { COLORS } from "../utils/colors";
+
 import TextArea from "../components/TextArea";
 import Buttons from "../components/Button";
+import FeedbackSvg from "../components/svg/Feedback";
+import { STYLES } from "../utils/styles";
 
+const MAX_CHARACTERS = 180;
 export default class Feedback extends React.Component {
-  static propTypes = {
-    prop1: PropTypes.string,
-    prop2: PropTypes.number.isRequired,
-    prop3: PropTypes.func,
-  };
+  // static propTypes = {
+  //   prop1: PropTypes.string,
+  //   prop2: PropTypes.number.isRequired,
+  //   prop3: PropTypes.func,
+  // };
+
+  state = {
+    text: "",
+    loading: false,
+    error: false,
+  }
+
+  sendFeedback = () => {
+    const {text} = this.state;
+    console.log(text); // check terminal!
+    // TODO
+    // if text.length > MAX_CHARACTERS then setState({error: true})
+    // otherwise, submit the feedback and setState({loading: true, error: false})
+    // if feedback submitted successfully then go to previous screen (Home screen/Question screen)
+    // otherwise show setState({loading: false, error: true})
+  }
 
   render() {
-    // const { ... } = this.props;
+    const { loading, error, text } = this.state;
 
     return (
-
-      <SafeAreaView style={styles.container}>
-
-        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}  style={styles.container}>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <ScrollView>
-            <Text
-              style={{
-                fontSize: 60,
-                fontWeight: "bold",
-                color: COLORS.primaryText,
-                marginTop: 60,
-                textAlign: "center",
-                flex: 1,
-              }}
-            >
-              ماذا تقترح؟
-            </Text>
-
-            <FeedbackSvg height={350} style={{ aspectRatio: 1 }} />
-            <TextArea isRtl={true} fontSize={18} max={180} placeholder="ماذا تقترح؟" />
-
-            <View style={[styles.buttonContainer]}>
-              <Buttons title="ارسال" fontSize={25} />
+      <SafeAreaView style={{ flex: 1 }}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={STYLES.mainContainer}
+        >
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+          >
+            <View style={[STYLES.titleContainer, {marginBottom: 15}]}>
+              <Text style={STYLES.title}>ماذا تقترح؟</Text>
+              <FeedbackSvg />
             </View>
 
+            <TextArea
+              isRtl={true}
+              fontSize={18}
+              max={MAX_CHARACTERS}
+              placeholder="ماذا تقترح؟"
+              value={text}
+              onChangeText={text => this.setState({text})}
+            />
+
+            <View style={[styles.buttonContainer]}>
+              <Buttons title="إرسال" fontSize={25} onPress={this.sendFeedback} />
+            </View>
           </ScrollView>
-          </TouchableWithoutFeedback>
         </KeyboardAvoidingView>
       </SafeAreaView>
     );
@@ -65,15 +76,8 @@ export default class Feedback extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    //marginTop: 20,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    marginTop: StatusBar.currentHeight,
-  },
   buttonContainer: {
     marginTop: 15,
-    alignSelf: "center"
+    alignSelf: "center",
   },
 });
