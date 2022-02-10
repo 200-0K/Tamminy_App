5fd3469631882e6c50fbbd006476e25978ab869a
Adel <Adal66777@gmail.com>
2022-02-10 22:06:45 +0300

    edit_3

--- screens/Question.js
+++ screens/Question.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, ScrollView, SafeAreaView, TouchableOpacity, TouchableHighlight, ActivityIndicator, Animated, Alert } from 'react-native';
+import { StyleSheet, View, Text, ScrollView, SafeAreaView, TouchableOpacity, ActivityIndicator, Animated } from 'react-native';
 import React from 'react';
 import { MaterialIcons } from '@expo/vector-icons';
 
@@ -14,118 +14,129 @@ import chroma from "chroma-js";
 // node_modules
 // yarn add react-native-linear-gradient
 // yarn remove react-native-linear-gradient
-
 // expo install expo-linear-gradient@~11.0.3
 
 export default class Question extends React.Component {
+  TOTAL_QUESTION = 0;
+  SELECTED_SYMPTOMS = [];
+
   state = {
     loading: true,
     error: false,
-    questions: [
+    newQuestions: [
+      {
+        id: 3,
+        question: "هل تعاني من حمى؟",
+        answer: null
+      },
       {
         id: 1,
         question: "هل تشعر بصداع؟",
+        answer: null
       },
       {
         id: 2,
         question: "هل لديك كحة؟",
-      },
-      {
-        id: 3,
-        question: "هل تعاني من حمى؟",
-      },
-
-      {
-        id: 1,
-        question: "هل تشعر بصداع؟",
-      },
-      {
-        id: 2,
-        question: "هل لديك كحة؟",
-      },
-      {
-        id: 3,
-        question: "هل تعاني من حمى؟",
-      },
-
-      {
-        id: 1,
-        question: "هل تشعر بصداع؟",
-      },
-      {
-        id: 2,
-        question: "هل لديك كحة؟",
-      },
-      {
-        id: 3,
-        question: "هل تعاني من حمى؟",
+        answer: null
       },
     ],
-    symptomsId: [],
-
-    questionsToAnswer: [
-      {
-        id: 3,
-        question: "هل تعاني من حمى؟",
-      },
-    ],
-    answeredQuestions: [
-      {
-        id: 1,
-        question: "هل تشعر بصداع؟",
-        isYes: true
-      },
-      {
-        id: 2,
-        question: "هل لديك كحة؟",
-        isYes: false
-      },
-    ],
-    currentQuestionIndex: 0,
+    oldQuestions: [],
     progressBarValue: new Animated.Value(0),
   }
 
   async componentDidMount() {
-    // const newSympotms = await AssessmentApi.getRelatedSymptoms(symptoms) 
-    // this.setState(newSympotms)
+    // TODO
+    try {
+      // SELECTED_SYMPTOMS = Get selected symptoms from SymptomSearch screen
+
+      // const newQuestions = this.getNewQuestions()
+      const newQuestions = this.state.newQuestions; //?TO BE CHANGED
+
+      this.TOTAL_QUESTION = newQuestions.length;
+      this.setState({
+        loading: false,
+        error: false,
+        newQuestions,
+      });
+    } catch (e) {
+      this.setState({
+        loading: false,
+        error: true,
+      })
+    }
+  }
+
+  getNewQuestions = () => {
+    //TODO: get Related Symptoms Question
+    // const newQuestions = await AssessmentApi.getRelatedSymptomsQuestion(SELECTED_SYMPTOMS, old questions with yes answer)
+    // return newQuestions;
+  }
+
+  navigateToAssessmentDetail = () => {
+    const { newQuestions, oldQuestions } = this.state;
+    // TODO navigate to navigate to AssessmentDetail screen with the selectedSymptoms
     this.setState({
-      loading: false,
-      error: false,
+      newQuestions: [...oldQuestions],
+      oldQuestions: [],
+    }, () => {
+      this.updateProgressBar();
     })
   }
 
-  updateCurrentQuestionIndex = () => {
-    const {currentQuestionIndex, questions, progressBarValue} = this.state
+  handleFeedbackPress = () => {
+    const { newQuestions } = this.state;
+    // TODO: navigate to feedback screen with question id
+    // Navigate("feedback", newQuestions[0].id)
+  }
+
+  handleQuestionInfoPress = () => {
+    const { id } = this.state.newQuestions[0];
+    // TODO: navigate to SymptomDetail screen with (symptom id == question id)
+  }
+
+  updateProgressBar = () => {
+    const { newQuestions, progressBarValue } = this.state
     Animated.timing(progressBarValue, {
-      toValue: Math.round((currentQuestionIndex/questions.length)*10)/10,
+      toValue: 1 - (newQuestions.length / this.TOTAL_QUESTION),
       duration: 750,
     }).start();
   }
 
-  handleYesAnswer = () => {
-    const { currentQuestionIndex, questions, symptomsId } = this.state;
-    const question = questions[currentQuestionIndex];
-    const newSymptomsId = [...symptomsId, question.id]
+  // function handleAnswer(answer) { }
+  handleAnswer = answer => {
+    const { newQuestions, oldQuestions } = this.state;
 
-    // TODO
-    // this.setState({loading: true})
-    // newQuestions = await (call server with symptomsId)
-    // this.setState({questions: newQuestions, symptomsId})
+    const currentQuestion = newQuestions[0];
+    currentQuestion.answer = answer;
+
+    const oldQs = [...oldQuestions, currentQuestion]
+
+    let newQs = newQuestions.slice(1);
+    // if (answer == true) {
+    //   newQs = this.getNewQuestions();
+    // }
 
     this.setState({
-      currentQuestionIndex: (currentQuestionIndex+1) % questions.length, //! REMOVE
-      symptomsId,
-      // questions: newQuesId: newSymptomstions
+      loading: false,
+      error: false,
+      newQuestions: newQs,
+      oldQuestions: oldQs,
     }, () => {
-      this.updateCurrentQuestionIndex();
+      this.updateProgressBar();
     })
   }
 
   render() {
     // const { id, question, SymptomDescription } = questionDiseases;
-    const { loading, error, questions, currentQuestionIndex, progressBarValue } = this.state;
+    const { loading, error, newQuestions, progressBarValue } = this.state;
     this.rtlText = STYLES.rtlText;
     this.rtlView = STYLES.rtlView;
+
+    // TODO: Good?
+    if (newQuestions.length < 1) {
+      this.navigateToAssessmentDetail();
+      return null
+    }
 
     if (loading) {
       // TODO: custom component
@@ -161,10 +172,8 @@ export default class Question extends React.Component {
       );
     }
 
-    const q = questions[currentQuestionIndex];
-
     return (
-      <SafeAreaView style={{ flex: 1}}>
+      <SafeAreaView style={{ flex: 1 }}>
         <ScrollView
           showsVerticalScrollIndicator={false}
           contentContainerStyle={STYLES.mainContainer}
@@ -176,8 +185,10 @@ export default class Question extends React.Component {
           </View>
           <View style={{ flex: 1 }}>
             <View>
-              <Text style={[styles.question, this.rtlText]}>{q.question}</Text>
-              <TouchableOpacity >
+              <Text style={[styles.question, this.rtlText]}>{newQuestions[0].question}</Text>
+              <TouchableOpacity
+                onPress={this.handleQuestionInfoPress}
+              >
                 <Text style={{
                   fontSize: 15,
                   color: COLORS.secondaryText,
@@ -194,7 +205,7 @@ export default class Question extends React.Component {
                 icon='checkmark-sharp'
                 iconSize='large'
                 iconColor={COLORS.checkmark}
-                onPress={this.handleYesAnswer}
+                onPress={() => this.handleAnswer(true)}
               />
             </View>
             <View style={{
@@ -210,7 +221,7 @@ export default class Question extends React.Component {
                 icon='close-sharp'
                 iconSize='large'
                 iconColor={COLORS.close}
-                onPress={this.handleNoAnswer}
+                onPress={() => this.handleAnswer(false)}
               />
             </View>
           </View>
@@ -220,7 +231,7 @@ export default class Question extends React.Component {
             padding: 10
           }}>
 
-            <TouchableOpacity style={styles.feedbackContainer}>
+            <TouchableOpacity style={styles.feedbackContainer} onPress={this.handleFeedbackPress}>
               <Feedback size={30} color={COLORS.buttonText} />
             </TouchableOpacity>
           </View>
