5a21ca7447e9eacf64799f8b175b62730a3dbb98
Faisal S.A <Ksa45560@gmail.com>
2022-02-10 23:21:52 +0300

    format + small fixes

--- screens/Question.js
+++ screens/Question.js
@@ -1,15 +1,20 @@
-import { StyleSheet, View, Text, ScrollView, SafeAreaView, TouchableOpacity, ActivityIndicator, Animated } from 'react-native';
-import React from 'react';
-import { MaterialIcons } from '@expo/vector-icons';
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  SafeAreaView,
+  TouchableOpacity,
+  ActivityIndicator,
+  Animated,
+} from "react-native";
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
 
-import Button from "../components/Button";
 import { COLORS } from "../utils/colors";
-import { formatDate } from "../utils/date";
 import { STYLES } from "../utils/styles";
-import { LinearGradient } from 'expo-linear-gradient';
 import Feedback from "../components/svg/icons/Feedback";
-import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
-import chroma from "chroma-js";
+import Button from "../components/Button";
 
 // node_modules
 // yarn add react-native-linear-gradient
@@ -27,22 +32,22 @@ export default class Question extends React.Component {
       {
         id: 3,
         question: "هل تعاني من حمى؟",
-        answer: null
+        answer: null,
       },
       {
         id: 1,
         question: "هل تشعر بصداع؟",
-        answer: null
+        answer: null,
       },
       {
         id: 2,
         question: "هل لديك كحة؟",
-        answer: null
+        answer: null,
       },
     ],
     oldQuestions: [],
     progressBarValue: new Animated.Value(0),
-  }
+  };
 
   async componentDidMount() {
     // TODO
@@ -62,45 +67,49 @@ export default class Question extends React.Component {
       this.setState({
         loading: false,
         error: true,
-      })
+      });
     }
   }
 
   getNewQuestions = () => {
     //TODO: get Related Symptoms Question
-    // const newQuestions = await AssessmentApi.getRelatedSymptomsQuestion(SELECTED_SYMPTOMS, old questions with yes answer)
+    // const selectedSymptoms = this.props.navigation.state.params?.selectedSymptoms || [];
+    // const newQuestions = await AssessmentApi.getRelatedSymptomsQuestion([...selectedSymptoms, old questions id with yes answer])
     // return newQuestions;
-  }
+  };
 
   navigateToAssessmentDetail = () => {
-    const { newQuestions, oldQuestions } = this.state;
+    const { oldQuestions } = this.state;
     // TODO navigate to navigate to AssessmentDetail screen with the selectedSymptoms
-    this.setState({
-      newQuestions: [...oldQuestions],
-      oldQuestions: [],
-    }, () => {
-      this.updateProgressBar();
-    })
-  }
+    this.setState(
+      {
+        newQuestions: [...oldQuestions],
+        oldQuestions: [],
+      },
+      () => {
+        this.updateProgressBar();
+      }
+    );
+  };
 
   handleFeedbackPress = () => {
-    const { newQuestions } = this.state;
+    const { id } = this.state.newQuestions[0];
     // TODO: navigate to feedback screen with question id
     // Navigate("feedback", newQuestions[0].id)
-  }
+  };
 
   handleQuestionInfoPress = () => {
     const { id } = this.state.newQuestions[0];
     // TODO: navigate to SymptomDetail screen with (symptom id == question id)
-  }
+  };
 
   updateProgressBar = () => {
-    const { newQuestions, progressBarValue } = this.state
+    const { newQuestions, progressBarValue } = this.state;
     Animated.timing(progressBarValue, {
-      toValue: 1 - (newQuestions.length / this.TOTAL_QUESTION),
+      toValue: 1 - newQuestions.length / this.TOTAL_QUESTION,
       duration: 750,
     }).start();
-  }
+  };
 
   // function handleAnswer(answer) { }
   handleAnswer = answer => {
@@ -109,34 +118,31 @@ export default class Question extends React.Component {
     const currentQuestion = newQuestions[0];
     currentQuestion.answer = answer;
 
-    const oldQs = [...oldQuestions, currentQuestion]
+    const oldQs = [...oldQuestions, currentQuestion];
 
     let newQs = newQuestions.slice(1);
     // if (answer == true) {
     //   newQs = this.getNewQuestions();
     // }
 
-    this.setState({
-      loading: false,
-      error: false,
-      newQuestions: newQs,
-      oldQuestions: oldQs,
-    }, () => {
-      this.updateProgressBar();
-    })
-  }
+    this.setState(
+      {
+        loading: false,
+        error: false,
+        newQuestions: newQs,
+        oldQuestions: oldQs,
+      },
+      () => {
+        this.updateProgressBar();
+      }
+    );
+  };
 
   render() {
     // const { id, question, SymptomDescription } = questionDiseases;
     const { loading, error, newQuestions, progressBarValue } = this.state;
     this.rtlText = STYLES.rtlText;
     this.rtlView = STYLES.rtlView;
-
-    // TODO: Good?
-    if (newQuestions.length < 1) {
-      this.navigateToAssessmentDetail();
-      return null
-    }
 
     if (loading) {
       // TODO: custom component
@@ -172,70 +178,83 @@ export default class Question extends React.Component {
       );
     }
 
+    // TODO: Good?
+    if (newQuestions.length < 1) {
+      this.navigateToAssessmentDetail();
+      return null;
+    }
+
     return (
       <SafeAreaView style={{ flex: 1 }}>
+        <View style={[styles.progressBarContainer]}>
+          <Animated.View
+            style={[styles.progressBar, { flex: progressBarValue }]}
+          >
+            <LinearGradient
+              colors={["#3F3D56", "#FF057D"]}
+              style={{ flex: 1 }}
+              start={{ x: 0, y: 0.5 }}
+              end={{ x: 1, y: 0.5 }}
+            ></LinearGradient>
+          </Animated.View>
+        </View>
+
         <ScrollView
           showsVerticalScrollIndicator={false}
           contentContainerStyle={STYLES.mainContainer}
         >
-          <View style={[styles.barContainer]}>
-            <Animated.View style={[styles.parogressBar, { flex: progressBarValue }]}>
-              <LinearGradient colors={["#3F3D56", "#FF057D"]} style={{ flex: 1 }} start={{ x: 0, y: .5 }} end={{ x: 1, y: .5 }}></LinearGradient>
-            </Animated.View>
-          </View>
           <View style={{ flex: 1 }}>
-            <View>
-              <Text style={[styles.question, this.rtlText]}>{newQuestions[0].question}</Text>
-              <TouchableOpacity
-                onPress={this.handleQuestionInfoPress}
-              >
-                <Text style={{
-                  fontSize: 15,
-                  color: COLORS.secondaryText,
-                  textAlign: 'right'
-                }}>ماذا تعني؟</Text>
+            <View style={styles.questionContainer}>
+              <Text style={[styles.question, this.rtlText]}>
+                {newQuestions[0].question}
+              </Text>
+              <TouchableOpacity onPress={this.handleQuestionInfoPress}>
+                <Text
+                  style={{
+                    fontSize: 15,
+                    color: COLORS.secondaryText,
+                    textAlign: "right",
+                  }}
+                >
+                  ماذا تعني؟
+                </Text>
               </TouchableOpacity>
             </View>
-            <View style={styles.buttonAnswer}>
+
+            <View style={styles.answerButtonContainer}>
               <Button
-                title='نعم'
+                title="نعم"
                 isRtl={true}
-                borderRadius={20}
+                borderRadius={15}
                 width={95}
-                icon='checkmark-sharp'
-                iconSize='large'
+                icon="checkmark-sharp"
+                iconSize="large"
                 iconColor={COLORS.checkmark}
                 onPress={() => this.handleAnswer(true)}
+                fontSize={24}
               />
             </View>
-            <View style={{
-              alignSelf: 'flex-end',
-              paddingHorizontal: 5,
-              paddingTop: 8
-            }}>
+            <View style={styles.answerButtonContainer}>
               <Button
-                title='لا'
+                title="لا"
                 isRtl={true}
-                borderRadius={20}
+                borderRadius={15}
                 width={95}
-                icon='close-sharp'
-                iconSize='large'
+                icon="close-sharp"
+                iconSize="large"
                 iconColor={COLORS.close}
                 onPress={() => this.handleAnswer(false)}
+                fontSize={24}
               />
             </View>
           </View>
 
-          <View style={{
-            alignSelf: 'flex-end',
-            padding: 10
-          }}>
-
-            <TouchableOpacity style={styles.feedbackContainer} onPress={this.handleFeedbackPress}>
-              <Feedback size={30} color={COLORS.buttonText} />
-            </TouchableOpacity>
-          </View>
-
+          <TouchableOpacity
+            style={styles.feedbackContainer}
+            onPress={this.handleFeedbackPress}
+          >
+            <Feedback size={25} color={COLORS.buttonText} />
+          </TouchableOpacity>
         </ScrollView>
       </SafeAreaView>
     );
@@ -243,38 +262,41 @@ export default class Question extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    alignSelf: 'center',
+  progressBarContainer: {
+    overflow: "hidden",
+    marginTop: 50,
+    marginBottom: 100,
+    flexDirection: "row",
+    borderRadius: 0,
+    borderWidth: 0,
+  },
+  progressBar: {
+    overflow: 'hidden',
+    flex: 0,
+    height: 20,
+    borderTopEndRadius: 20,
+    borderBottomEndRadius: 20,
+  },
+
+  questionContainer: {
+    paddingVertical: 20,
   },
   question: {
     fontSize: 50,
     fontWeight: "bold",
-    color: COLORS.primaryText
-  },
-  barContainer: {
-    marginTop: 50,
-    marginBottom: 100,
-    flexDirection: "row",
-    borderRadius: 10,
-    borderWidth: 1,
-    overflow: "hidden",
-  },
-  parogressBar: {
-    flex: 0,
-    height: 20,
+    color: COLORS.primaryText,
   },
 
-  buttonText: {
-    fontSize: 18,
-    textAlign: 'center',
-    margin: 10,
-    color: '#ffffff',
-    backgroundColor: 'transparent',
+  answerButtonContainer: {
+    alignSelf: "flex-end",
+    paddingVertical: 8,
+    paddingHorizontal: 5,
   },
 
-  buttonAnswer: {
-    alignSelf: 'flex-end',
-    paddingTop: 40,
-    paddingHorizontal: 5
-  }
+  feedbackContainer: {
+    alignSelf: "flex-end",
+    padding: 10,
+    paddingBottom: 15,
+    opacity: .7
+  },
 });
