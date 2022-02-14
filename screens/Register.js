2b85a96af6e047c2a6d93b801c5bca8689d7f9c4
Faisal S.A <Ksa45560@gmail.com>
2022-02-14 13:53:54 +0300

    added ScreenWrapper

--- screens/Register.js
+++ screens/Register.js
@@ -1,13 +1,10 @@
 import React from "react";
 import {
-  SafeAreaView,
   StyleSheet,
   Text,
   View,
   ScrollView,
-  KeyboardAvoidingView,
   TouchableOpacity,
-  Alert,
 } from "react-native";
 import PropTypes from "prop-types";
 import { Ionicons } from "@expo/vector-icons";
@@ -20,8 +17,10 @@ import { STYLES } from "../utils/styles";
 import { COLORS } from "../utils/colors";
 import { formatDate } from "../utils/date";
 import chroma from "chroma-js";
+import ScreenWrapper from "../components/ScreenWrapper";
 
-const activeColor = "#e3e0f8";
+const activeColor = chroma(COLORS.primaryText).brighten(3.5).toString();
+const maximumDate = new Date(new Date().getFullYear() - 5, 11, 31);
 export default class ComponentName extends React.Component {
   // static propTypes = {
   //   prop1: PropTypes.string,
@@ -42,129 +41,118 @@ export default class ComponentName extends React.Component {
     email: null,
     password: null,
     open: false,
-    date: new Date(),
+    date: null,
   };
 
   handleCalendarPress = () => {
     this.setState({ open: true });
   };
-  
+
   handleSubmit = () => {
     const { date, gender, email, password, name } = this.state;
-
-    Alert.alert("Credentials", `
-      name: ${name}
-      email: ${email}
-      gender: ${gender}
-      DOB: ${date}
-      password: ${password}
-    `)
-  }
+    // TODO
+  };
 
   render() {
     const { open, date, gender, email, password, name } = this.state;
-    
+
     return (
-      <SafeAreaView style={STYLES.safeAreaView}>
+      <ScreenWrapper style={STYLES.safeAreaView}>
         <DateTimePickerModal
           isVisible={open}
           mode="date"
-          date={date}
+          date={date ?? maximumDate}
+          maximumDate={maximumDate}
           onConfirm={date => this.setState({ open: false, date })}
           onCancel={() => this.setState({ open: false })}
         />
 
-        <KeyboardAvoidingView
-          behavior={Platform.OS === "ios" ? "padding" : null}
-          style={STYLES.mainContainer}
-        >
-          <ScrollView showsVerticalScrollIndicator={false}>
-            <View style={STYLES.titleContainer}>
-              <Text style={STYLES.title}>تسـجـيل</Text>
-              <RegisterSvg />
-            </View>
+        <ScrollView showsVerticalScrollIndicator={false} style={STYLES.mainContainer}>
+          <View style={STYLES.titleContainer}>
+            <Text style={STYLES.title}>تسـجـيل</Text>
+            <RegisterSvg />
+          </View>
 
-            <View style={styles.Inputs}>
-              <TextInput
-                onChangeText={text => this.setState({ name: text })}
-                value={name}
-                icon="person"
-                isRtl
-                placeholder="الاسم"
-              />
-            </View>
+          <View style={styles.Inputs}>
+            <TextInput
+              onChangeText={text => this.setState({ name: text })}
+              value={name}
+              icon="person"
+              isRtl
+              placeholder="الاسم"
+            />
+          </View>
 
-            <View style={[styles.GenderFieldContainer, this.rtlView]}>
-              <Text
-                style={{
-                  fontSize: 18,
-                  fontWeight: "bold",
-                  color: "rgba(76,74,94,0.7)",
-                }}
-              >
-                الجنس :
-              </Text>
-              <View style={[styles.Inputs, styles.GenderButtonsContainer]}>
-                <Button
-                  width={100}
-                  borderRadius={5}
-                  title="انثى"
-                  backgroundColor={
-                    gender === "f" ? activeColor : COLORS.buttonBackground
-                  }
-                  onPress={() => this.setState({ gender: "f" })}
-                />
-                <Button
-                  width={100}
-                  borderRadius={5}
-                  title="ذكر"
-                  backgroundColor={
-                    gender === "m" ? activeColor : COLORS.buttonBackground
-                  }
-                  onPress={() => this.setState({ gender: "m" })}
-                />
-              </View>
-            </View>
-
-            <TouchableOpacity
-              onPress={this.handleCalendarPress}
-              style={[styles.Inputs, styles.dateContainer, this.rtlView]}
+          <View style={[styles.GenderFieldContainer, this.rtlView]}>
+            <Text
+              style={{
+                fontSize: 18,
+                fontWeight: "bold",
+                color: "rgba(76,74,94,0.7)",
+              }}
             >
-              <Ionicons name="calendar" size={20} />
-              <View style={styles.dateTextContainer}>
-                <Text style={[styles.dateText, this.rtlText]}>
-                  {formatDate(date)}
-                </Text>
-              </View>
-            </TouchableOpacity>
-
-            <View style={styles.Inputs}>
-              <TextInput
-                onChangeText={text => this.setState({ email: text })}
-                value={email}
-                icon="at"
-                isRtl
-                placeholder="الايميل"
+              الجنس :
+            </Text>
+            <View style={[styles.Inputs, styles.GenderButtonsContainer]}>
+              <Button
+                width={100}
+                borderRadius={5}
+                title="انثى"
+                backgroundColor={
+                  gender === "f" ? activeColor : COLORS.buttonBackground
+                }
+                onPress={() => this.setState({ gender: "f" })}
+              />
+              <Button
+                width={100}
+                borderRadius={5}
+                title="ذكر"
+                backgroundColor={
+                  gender === "m" ? activeColor : COLORS.buttonBackground
+                }
+                onPress={() => this.setState({ gender: "m" })}
               />
             </View>
+          </View>
 
-            <View style={styles.Inputs}>
-              <TextInput
-                icon="md-key-outline"
-                isRtl
-                secureTextEntry={true}
-                placeholder="كلمة المرور"
-                onChangeText={text => this.setState({ password: text })}
-                value={password}
-              />
+          <TouchableOpacity
+            onPress={this.handleCalendarPress}
+            style={[styles.Inputs, styles.dateContainer, this.rtlView]}
+          >
+            <Ionicons name="calendar" size={20} />
+            <View style={styles.dateTextContainer}>
+              <Text style={[styles.dateText, this.rtlText]}>
+                {date ? formatDate(date) : "تاريخ الميلاد"}
+              </Text>
             </View>
+          </TouchableOpacity>
 
-            <View style={styles.ButtonStyle}>
-              <Button onPress={this.handleSubmit} title="تسجيل" />
-            </View>
-          </ScrollView>
-        </KeyboardAvoidingView>
-      </SafeAreaView>
+          <View style={styles.Inputs}>
+            <TextInput
+              onChangeText={text => this.setState({ email: text })}
+              value={email}
+              icon="at"
+              isRtl
+              placeholder="الايميل"
+            />
+          </View>
+
+          <View style={styles.Inputs}>
+            <TextInput
+              icon="md-key-outline"
+              isRtl
+              secureTextEntry={true}
+              placeholder="كلمة المرور"
+              onChangeText={text => this.setState({ password: text })}
+              value={password}
+            />
+          </View>
+
+          <View style={styles.ButtonStyle}>
+            <Button onPress={this.handleSubmit} title="تسجيل" />
+          </View>
+        </ScrollView>
+      </ScreenWrapper>
     );
   }
 }
