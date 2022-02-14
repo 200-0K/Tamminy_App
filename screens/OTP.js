f85d29e88642e8c8b1bccafdd7999b3c279a6be2
Faisal S.A <Ksa45560@gmail.com>
2022-02-14 13:19:39 +0300

    fix OTP screen | resolve #10

--- screens/OTP.js
+++ screens/OTP.js
@@ -1,96 +1,86 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
 import {
-  SafeAreaView,
   StyleSheet,
   Text,
   View,
   ScrollView,
-  KeyboardAvoidingView,
-  Platform,
-  Keyboard,
-  TouchableWithoutFeedback
 } from "react-native";
-
 import PropTypes from "prop-types";
+import OTPInputView from "@twotalltotems/react-native-otp-input";
+
 import OTPSvg from "../components/svg/OTP";
+import Button from "../components/Button";
+
 import { COLORS } from "../utils/colors";
-import Buttons from "../components/Button";
-import OTPInputView from '@twotalltotems/react-native-otp-input'
+import { STYLES } from "../utils/styles";
+import ScreenWrapper from "../components/ScreenWrapper";
+
 export default class Feedback extends React.Component {
-  static propTypes = {
-    prop1: PropTypes.string,
-    prop2: PropTypes.number.isRequired,
-    prop3: PropTypes.func,
+  // static propTypes = {
+  //   prop1: PropTypes.string,
+  //   prop2: PropTypes.number.isRequired,
+  //   prop3: PropTypes.func,
+  // };
+
+  state = {
+    otp: null,
   };
 
+  verifyOtp = () => {
+
+  }
+
   render() {
-    // const { ... } = this.props;
+    const { otp } = this.state;
 
     return (
+      <ScreenWrapper>
+        <ScrollView style={STYLES.mainContainer}>
+          <View style={STYLES.titleContainer}>
+            <Text style={STYLES.title}>رمز التحقق</Text>
+          </View>
 
-      <SafeAreaView style={styles.container}>
+          <OTPSvg />
+          <OTPInputView
+            style={[styles.field, styles.otpInputView]}
+            pinCount={4}
+            keyboardType="phone-pad"
+            placeholderCharacter="*"
+            placeholderTextColor="rgba(0,0,0,0.2)"
+            autoFocusOnLoad
+            codeInputFieldStyle={styles.codeInputFieldStyle}
+            codeInputHighlightStyle={styles.codeInputHighlightStyle}
+            onCodeFilled={code => this.setState({otp: code})}
+          />
 
-        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.container}>
-          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <ScrollView>
-              <Text
-                style={{
-                  fontSize: 60,
-                  fontWeight: "bold",
-                  color: COLORS.primaryText,
-                  marginTop: 60,
-                  textAlign: "center",
-                  flex: 1,
-                }}
-              >
-                رمز التحقق
-              </Text>
-
-              <OTPSvg height={315} style={{ aspectRatio:1.3, marginTop: 30 }} />
-
-              <OTPInputView
-                style={{ width: '500%', height: 100, marginTop:50 }, styles.container}
-                pinCount={4}
-                autoFocusOnLoad
-                codeInputFieldStyle={styles.underlineStyleBase}
-                codeInputHighlightStyle={styles.underlineStyleHighLighted}
-              />
-
-              <View style={[styles.buttonContainer]}>
-                <Buttons title="تحقق" fontSize={25} />
-              </View>
-
-            </ScrollView>
-          </TouchableWithoutFeedback>
-        </KeyboardAvoidingView>
-      </SafeAreaView>
+          <View style={[styles.field, styles.buttonContainer]}>
+            <Button title="تحقق" fontSize={25} width={100} onPress={this.verifyOtp} />
+          </View>
+        </ScrollView>
+      </ScreenWrapper>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    marginTop: StatusBar.currentHeight,
+  field: {
+    marginVertical: 20,
   },
   buttonContainer: {
-    marginTop: 70,
-    alignSelf: "center"
+    alignSelf: "center",
   },
-  borderStyleBase: {
-    width: 40,
-    height: 45,
+
+  otpInputView: {
+    flex: 1,
   },
-  underlineStyleBase: {
-    width: 45,
-    height: 50,
+  codeInputFieldStyle: {
     borderWidth: 0,
-    borderBottomWidth:3,
+    borderBottomWidth: 3,
+    fontSize: 24,
+    color: COLORS.secondaryText,
   },
-  underlineStyleHighLighted: {
-    borderColor: "red",
-    
+  codeInputHighlightStyle: {
+    borderColor: COLORS.otpUnderline,
+    color: COLORS.primaryText,
   },
 });
