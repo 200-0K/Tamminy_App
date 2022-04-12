1f3e69c9935e729dad6f52d26b13ad4f383a12fa
Faisal S.A <Ksa45560@gmail.com>
2022-04-12 12:14:49 +0300

    ApiManager is complete | resolve #71

---
+++ api/ApiManager.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { AxiosInstance } from "axios";
+
+const updateAxiosToken = (axiosInstance, token, tokenChangedCallback = () => {}) => {
+  axiosInstance.defaults.headers["Authorization"] = token ? `Bearer ${token}` : "";
+  tokenChangedCallback(token);
+}
+
+let isInstantiated = false; // important
+let axiosInstance;
+/**
+ * A singleton function for getting an instance of an `ApiManager`
+ * @param {{token:string, baseUrl:string, tokenChangedCallback:function}} options only used for first initilaizing
+ * @returns {AxiosInstance}
+ */
+export const ApiManager = options => {
+  if (isInstantiated) {
+    if (options) throw new Error("Instance has already been instantiated")
+    return axiosInstance;
+  }
+
+  axiosInstance = axios.create({
+    baseURL: options.baseUrl,
+  })
+  if (options.token) updateAxiosToken(axiosInstance, options.token)
+
+  axiosInstance.interceptors.response.use(
+    response => {
+      if (response.data.token) updateAxiosToken(axiosInstance, response.data.token, options.tokenChangedCallback)
+      
+      return response;
+    }, 
+    error => {
+      const status = error.response ? error.response.status : null
+
+      // TODO: if status=null then user/server is offline?
+
+      if (status === 401) {
+        updateAxiosToken(axiosInstance, null, options.tokenChangedCallback)
+      } else if (response.data.token) {
+        updateAxiosToken(axiosInstance, response.data.token, options.tokenChangedCallback)
+      }
+
+      return Promise.reject(error);
+    }
+  )
+
+  isInstantiated = true;
+  return axiosInstance;
+}
