189af6267d3c72cef87491ba9d74f923a9be7eea
Faisal S.A <Ksa45560@gmail.com>
2022-04-12 17:18:43 +0300

    resolve #73 + with tests

--- api/Api.test.js
+++ api/Api.test.js
@@ -1,11 +1,57 @@
 import { ApiManager } from "./ApiManager";
+import { AccountApi } from "./AccountApi";
 import { SymptomApi } from "./SymptomApi";
 import { DiseaseApi } from "./DiseaseApi";
 
 let apiManager;
 beforeAll(() => {
   apiManager = ApiManager({
-    baseUrl: "http://localhost",
+    baseUrl: "http://localhost/api/v1",
+  });
+});
+
+describe("Account End-Point", () => {
+  let accountApi;
+  beforeAll(() => {
+    accountApi = AccountApi(apiManager);
+  });
+
+  test("Instance already instantiated", () => {
+    expect(() => AccountApi({})).toThrow();
+  });
+
+  test("Get an instance", () => {
+    expect(AccountApi()).toBeDefined();
+  });
+
+  const data = {
+    email: `${Math.random().toString(32).slice(2)}@tester.com`,
+    password: "123123123465456464",
+    dob: new Date().toISOString(),
+    name: `Tester-${Math.random().toFixed(4).slice(2, 4)}`,
+    gender: ["male", "female"][Math.round(Math.random())],
+  };
+
+  test("Register user", async () => {
+    const res = await accountApi.register(data);
+    expect(res).toBe(accountApi.RESPONSE_CODES.register.success);
+  });
+
+  test("Verify user OTP [Incorrect]", async () => {
+    const res = await accountApi.verify(0);
+    expect(res).toBe(accountApi.RESPONSE_CODES.verify.incorrect);
+  });
+
+  test("Verify user OTP", async () => {
+    const res = await accountApi.verify(4000); //! Hardcoded OTP
+    expect(res).toBe(accountApi.RESPONSE_CODES.verify.success);
+  });
+
+  test("Login", async () => {
+    const prevToken = apiManager.defaults.headers.Authorization;
+    const res = await accountApi.login(data.email, data.password);
+    expect(res).toBe(accountApi.RESPONSE_CODES.login.success);
+    expect(apiManager.defaults.headers.Authorization).not.toEqual(prevToken);
   });
 });
 
