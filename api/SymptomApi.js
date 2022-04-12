ceef7b8a3f775225103f8549af6095ac6c070d8b
Faisal S.A <Ksa45560@gmail.com>
2022-04-12 13:56:19 +0300

    resolve #72

---
+++ api/SymptomApi.js
@@ -0,0 +1,33 @@
+import {AxiosInstance, AxiosResponse} from "axios";
+
+let isInstantiated = false;
+let instance;
+
+/**
+ * 
+ * @param {AxiosInstance} axios 
+ * @returns {{
+ *  getAllSymptoms: () => Promise<AxiosResponse<any, any>>,
+ *  getSymptom: (id: number) => Promise<AxiosResponse<any, any>>
+ * }}
+ */
+export const SymptomApi = axios => {
+  if (isInstantiated) {
+    if (axios) throw new Error("Instance has already been instantiated")
+    return instance;
+  }
+
+  instance = {
+    getAllSymptoms: async () => {
+      const res = await axios.get("/symptom/fetch/all");
+      return res.data;
+    },
+    getSymptom: async id => {
+      const res = await axios.get(`/symptom/fetch?id=${id}`);
+      return res.data;
+    }
+  }
+
+  isInstantiated = true;
+  return instance;
+}
