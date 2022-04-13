eac2abc223013713f0ded59d8fbe453856963729
Faisal S.A <Ksa45560@gmail.com>
2022-04-13 11:04:10 +0300

    ApiManager now implicity update the token (if needed) and re-send the request

--- api/ApiManager.js
+++ api/ApiManager.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 import { AxiosInstance } from "axios";
 
-const updateAxiosToken = (axiosInstance, token, tokenChangedCallback = () => {}) => {
-  axiosInstance.defaults.headers["Authorization"] = token ? `Bearer ${token}` : "";
+const updateAxiosToken = (
+  axiosInstance,
+  token,
+  tokenChangedCallback = () => {}
+) => {
+  axiosInstance.defaults.headers["Authorization"] = token
+    ? `Bearer ${token}`
+    : "";
   tokenChangedCallback(token);
-}
+};
 
 let isInstantiated = false; // important
 let axiosInstance;
@@ -15,36 +21,49 @@ let axiosInstance;
  */
 export const ApiManager = options => {
   if (isInstantiated) {
-    if (options) throw new Error("Instance has already been instantiated")
+    if (options) throw new Error("Instance has already been instantiated");
     return axiosInstance;
   }
 
   axiosInstance = axios.create({
     baseURL: options.baseUrl,
-  })
-  if (options.token) updateAxiosToken(axiosInstance, options.token)
+  });
+  if (options.token) updateAxiosToken(axiosInstance, options.token);
 
   axiosInstance.interceptors.response.use(
     response => {
-      if (response.data.token) updateAxiosToken(axiosInstance, response.data.token, options.tokenChangedCallback)
-      
+      if (response.data.token)
+        updateAxiosToken(
+          axiosInstance,
+          response.data.token,
+          options.tokenChangedCallback
+        );
       return response;
-    }, 
-    error => {
-      const status = error.response ? error.response.status : null
+    },
+    async error => {
+      const status = error.response ? error.response.status : null;
 
       // TODO: if status=null then user/server is offline?
 
       if (status === 401) {
-        updateAxiosToken(axiosInstance, null, options.tokenChangedCallback)
+        updateAxiosToken(axiosInstance, null, options.tokenChangedCallback);
       } else if (error.response.data.token) {
-        updateAxiosToken(axiosInstance, error.response.data.token, options.tokenChangedCallback)
+        updateAxiosToken(
+          axiosInstance,
+          error.response.data.token,
+          options.tokenChangedCallback
+        );
+        try {
+          error.config.headers["Authorization"] = axiosInstance.defaults.headers["Authorization"];
+          const res = await axios.request(error.config);
+          return res;
+        } catch (e) { error = e; }
       }
 
       return Promise.reject(error);
     }
-  )
+  );
 
   isInstantiated = true;
   return axiosInstance;
-}
+};
