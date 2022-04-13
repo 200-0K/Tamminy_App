0d364cea6cdde326078e5cc7b715b0df94ec7d9d
Faisal S.A <Ksa45560@gmail.com>
2022-04-13 22:04:27 +0300

    refactor Api.token.test

--- api/Api.token.test.js
+++ api/Api.token.test.js
@@ -1,11 +1,13 @@
 import { ApiManager } from "./ApiManager";
+import { AssessmentApi } from "./AssessmentApi";
 
-let apiManager = ApiManager({
+const apiManager = ApiManager({
   baseUrl: "http://localhost/api/v1",
   token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NDkxODc5NTcsImV4cCI6MTY0OTE4ODg1NywiZGF0YSI6eyJpZCI6NX19.alA7Qd5bzUadtvO1c3mXf0odAzDBtrvrF51W-nVCC6A"
-})
+});
+const assessmentApi = AssessmentApi(apiManager);
 
-test("ApiManager refresh token for user id 5", async () => {
-  const res = await apiManager.get("/assessment/fetch/all");
-  expect(res.status).toBe(200);
+test("Get all assessments for the expired token of user id 5", async () => {
+  const res = await assessmentApi.getAll();
+  expect(res).toBeDefined();
 });
