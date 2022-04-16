ca481768c5c01c98392038558966e06859addd2d
Faisal S.A <Ksa45560@gmail.com>
2022-04-17 00:47:11 +0300

    changed user id test for the expired token

--- api/Api.token.test.js
+++ api/Api.token.test.js
@@ -3,11 +3,11 @@ import { AssessmentApi } from "./AssessmentApi";
 
 const apiManager = ApiManager({
   baseUrl: "http://localhost/api/v1",
-  token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NDkxODc5NTcsImV4cCI6MTY0OTE4ODg1NywiZGF0YSI6eyJpZCI6NX19.alA7Qd5bzUadtvO1c3mXf0odAzDBtrvrF51W-nVCC6A"
+  token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NTAxNDU1NjMsImV4cCI6MTY1MDE0NjQ2MywiZGF0YSI6eyJpZCI6MX19.zrXpIsAqZn7ieTLRZi5Fl1SGx7PWScYyLdgqX56VgSA"
 });
 const assessmentApi = AssessmentApi(apiManager);
 
-test("Get all assessments for the expired token of user id 5", async () => {
+test("Get all assessments for the expired token of user id 1", async () => {
   const res = await assessmentApi.getAll();
   expect(res).toBeDefined();
 });
